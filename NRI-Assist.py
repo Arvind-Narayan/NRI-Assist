7dc8281eaac563e5769bfb5f126abd98a67ff807
Arvind-Narayan <92.arvind@gmail.com>
2024-06-13 22:45:41 +0530

    Update NRI-Assist.py

--- NRI-Assist.py
+++ NRI-Assist.py
@@ -83,9 +83,9 @@ with st.sidebar:
 
 
 
-st.title("NRI Assist 🇺🇸 👉 🇮🇳") 
+st.title("NRI Assist 🇺🇸 🇮🇳") 
 st.markdown("I am Arvind's **:blue[AI]** agent. You can talk to me as if you were talking to Arvind himself. \
-         Feel free to ask me questions about returning to India.")
+         Feel free to ask me questions about returning to India. ---")
 # st.markdown("You can talk to me as if you were talking to Arvind himself. \
 #          Feel free to ask me questions about returning to India.")
 st.write('---')
