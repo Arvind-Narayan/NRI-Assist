f94b759c3674a7302029348676e376b9020a991b
Arvind-Narayan <92.arvind@gmail.com>
2024-06-13 23:05:57 +0530

    Update About Arvind.py

--- pages/About Arvind.py
+++ pages/About Arvind.py
@@ -2,16 +2,20 @@ import streamlit as st
 
 st.set_page_config(page_title="About", page_icon="🇮🇳")
 
-# col1, col2= st.columns([10,50])
+st.header("About Me",divider='rainbow')
 
-# with col1:
-#    st.image('./img_arv.jpeg', )
+col1, col2, col3 = st.columns([1.3 ,0.2, 1])
 
-# with col2:  
-#     st.write("About Arvind ....")
+with col1:
+    st.write('ipsum lorem psum lorem psum lorem psum lorem psum lorem psum lorem psum lorem  \
+             psum lorem  psum lorem  psum lorem  psum lorem  psum lorem ')
+    
+with col3:
+    st.image("./img_arv.jpeg", width=360)
 
-st.image('./img_arv.jpeg', width = 500 )
-# st.image('./img_arv.jpeg')
-st.title("My Story")
 
-st.write("About Arvind ....")
+# st.image('./img_arv.jpeg', width = 500 )
+# # st.image('./img_arv.jpeg')
+# st.title("My Story")
+
+# st.write("About Arvind ....")
