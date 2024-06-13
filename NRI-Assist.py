3fed402b5f8d411a1b0383915f6ea0c571b33a27
Arvind-Narayan <92.arvind@gmail.com>
2024-06-13 22:41:45 +0530

    Update NRI-Assist.py

--- NRI-Assist.py
+++ NRI-Assist.py
@@ -84,10 +84,10 @@ with st.sidebar:
 
 
 st.title("NRI Assist 🇺🇸 👉 🇮🇳") 
-st.markdown("I am **Arvind's** :blue[AI] agent.")
-st.markdown("You can talk to me as if you were talking to Arvind himself. \
+st.markdown("I am **Arvind's** :blue[AI] agent. You can talk to me as if you were talking to Arvind himself. \
          Feel free to ask me questions about returning to India.")
-st.subheader('',divider = 'rainbow')
+# st.markdown("You can talk to me as if you were talking to Arvind himself. \
+#          Feel free to ask me questions about returning to India.")
 # st.write('---')
 
 #thread session state
