3a65b689d7bb1984c44e1767727a51ce186da578
Arvind-Narayan <92.arvind@gmail.com>
2024-06-19 20:57:40 +0530

    update limit

--- NRI-Assist.py
+++ NRI-Assist.py
@@ -136,7 +136,7 @@ if prompt:
         st.info("Please login to continue.")
         st.stop()
 
-    if user_info['msg_count'] >= 10:
+    if user_info['msg_count'] >= 9:
         st.info("Sorry we have reached the token limit. Please reach out to Arvind directly!")
         st.stop()
     
